340570e7fff0d398d0ca7866c4b8bf2321ea00da
Morten Bonderup <moab@eaaa.dk>
2023-09-04 07:58:40 +0200

    Version 1

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+    return (
+        <section className="page">
+            <h1>Home Page</h1>
+            <p>This is my HomePage Page.</p>
+        </section>
+    );
+}
