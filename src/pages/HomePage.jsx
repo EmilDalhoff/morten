254d499c698457c524fdc9c59859d7f4077e2089
Morten Bonderup <moab@eaaa.dk>
2023-09-05 13:07:57 +0200

    Version 1

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import Content from "../components/Content";
+
 export default function HomePage() {
+
+    const [posts, setposts] = useState([]);
+
+    useEffect(() => {
+        async function getPosts() {
+            const url = "http://192.168.10.10/wp-json/wp/v2/posts?_embed&categories=12";
+            const response = await fetch(url);
+            const data = await response.json();
+            setposts(data);
+        }
+        getPosts();
+    }, []);
+
     return (
-        <section className="page">
-            <h1>Home Page</h1>
-            <p>This is my HomePage Page.</p>
+        <section>
+            <h1>HomePage</h1>
+            {posts.map(post => (
+                <Content key={post.id} post={post} />
+            ))}
         </section>
-    );
+    )
 }
