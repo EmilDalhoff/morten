6a74279db703ccc3ccc27ae073e303ee0203f0d4
EmilDalhoff <emildalhoff@hotmail.com>
2023-09-20 13:55:54 +0200

    Portfolio
    Ændringer

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import AboutPage from "./pages/AboutPage";
 import Nav from "./components/Nav";
 import ContactPage from "./pages/ContactPage";
 
+
 function App() {
     return (
         <main>
+            
             <Nav />
             <Routes>
                 <Route path="/" element={<HomePage />} />
-                <Route path="/about" element={<AboutPage />} />
+                <Route path="#" element={<AboutPage />} />
                 <Route path="/contact" element={<ContactPage />} />
                 <Route path="*" element={<Navigate to="/" />} />
             </Routes>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,10 +2,17 @@ import { NavLink } from "react-router-dom";
 
 export default function Nav() {
     return (
+        
         <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
+            <img className="imgemil" src="src/assets/groenport.png" alt="nav-pic" />
+             <div className="nav-bar" >
+            <NavLink to="/">HJEM</NavLink>
+            <a href="#aboutme-link">OM MIG</a>
+            <a href="#port-link">PORTFOLIO</a> 
+            <a href="#skills-link">KOMPETENCER</a>
+            <a href="#contact-link">KONTAKT</a>
+            </div>
+
         </nav>
     );
 }
